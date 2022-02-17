d58d4900c6ab1634c3f139c9128e2075e699c27a
Tobin deKorne <tdekorne@gmail.com>
2022-02-16 17:02:16 -0800

    A fun particle system with p5js

---
+++ script.js
@@ -0,0 +1,85 @@
+const particles = [];
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+
+    const numParticles = Math.floor(window.innerWidth / 10);
+
+    for (let i = 0; i < numParticles; i++) {
+        particles.push(new Particle());
+    }
+}
+
+function windowResized() {
+    resizeCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+    clear();
+    particles.forEach((p, index) => {
+        p.update();
+        p.draw();
+        p.checkParticles(particles.slice(index));
+    });
+}
+
+class Particle {
+    constructor() {
+        this.pos = createVector(random(width), random(height));
+        this.size = 5;
+        this.connectionRange = 120;
+        const velFactor = 1.5;
+        this.vel = createVector(
+            random(-velFactor, velFactor),
+            random(-velFactor, velFactor)
+        );
+    }
+
+    draw() {
+        noStroke();
+        fill('rgba(255,255,255,0.3)');
+        circle(this.pos.x, this.pos.y, this.size);
+    }
+
+    update() {
+        this.pos.add(this.vel);
+        this.edges();
+    }
+
+    edges() {
+        if (this.pos.x < 0) {
+            this.pos.x = width;
+        }
+        if (this.pos.x > width) {
+            this.pos.x = 0;
+        }
+
+        if (this.pos.y < 0) {
+            this.pos.y = height;
+        }
+        if (this.pos.y > height) {
+            this.pos.y = 0;
+        }
+
+        // if (this.pos.y < 0 || this.pos.y > height) {
+        //     this.pos.y %= height;
+        // }
+    }
+
+    checkParticles(particles) {
+        particles.forEach((particle) => {
+            const d = dist(
+                this.pos.x,
+                this.pos.y,
+                particle.pos.x,
+                particle.pos.y
+            );
+
+            if (d < this.connectionRange) {
+                const a = 1 - d / this.connectionRange;
+                stroke(`rgba(255,255,255,${a})`);
+                strokeWeight(1 - d / this.connectionRange);
+                line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+            }
+        });
+    }
+}
